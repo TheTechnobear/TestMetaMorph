c8c1c18596bc878feff802a6a4c66e87d7aff98f
TheTechnobear <mharris@technobear.com>
2025-12-16 13:23:38 +0100

    remove sema dep, need mm implementation still

--- VcvModules/src/MMMidiDevice.h
+++ VcvModules/src/MMMidiDevice.h
@@ -5,25 +5,23 @@
 #include <memory>
 #include <vector>
 
-#include <readerwriterqueue.h>
-
-#include <atomic>
-
 
 class MMMidiDevice : public EraeApi::MidiDevice {
-
 public:
-    MMMidiDevice(unsigned inQueueSize = EraeApi::MidiDevice::MAX_QUEUE_SIZE, unsigned outQueueSize =EraeApi::MidiDevice::MAX_QUEUE_SIZE);
+    static constexpr int MAX_QUEUE_SIZE = 512;
+
+    MMMidiDevice(unsigned inQueueSize = MAX_QUEUE_SIZE, unsigned outQueueSize = MAX_QUEUE_SIZE);
     virtual ~MMMidiDevice();
     bool init(const char *indevice, const char *outdevice, bool virtualOutput = false) override { return false; }
     void deinit() override {;}
 
+    bool queueInMsg(const EraeApi::MidiMsg &msg) override { return false;}
+    bool queueOutMsg(const EraeApi::MidiMsg &msg) override { return false;}
 protected:
+    bool nextInMsg(EraeApi::MidiMsg &msg) override { return false;}
+    bool nextOutMsg(EraeApi::MidiMsg &msg) override { return false;}
 
     bool isOutputOpen() override { return false;}
     bool send(const EraeApi::MidiMsg &msg) override {return false;}
-
-    // std::unique_ptr<RtMidiIn> midiInDevice_;
-    // std::unique_ptr<RtMidiOut> midiOutDevice_;
 };
 
